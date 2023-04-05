27641d7932bd844d7da45663c92a60f41098285a
naga <thejus11@icloud.com>
2023-04-05 07:55:57 -0700

    added appClock

---
+++ apps/appClock/app.js
@@ -0,0 +1,35 @@
+//get the current time
+
+function getTime() {
+	
+	var date = new Date();
+	return{
+		hours: date.getHours(),
+		minutes: date.getMinutes(),
+		seconds: date.getSeconds()
+	};	
+}
+
+//draw the time and progress bar
+
+function drawTime(time) {
+	//clear the screen
+	g.clear();
+	
+	//draw the time
+	g.setFont("Vector",40);
+	g.drawString(
+		time.hours + ":" + ("0" + time.minutes).substr(-2) + ":" 
+		+ ("0" + time.seconds).substr(-2), 30, 30);
+	
+}
+	
+//update the time and draw the screen
+function updateScreen(){
+	var time= getTime();
+	drawTime(time);
+}
+
+// Set up the app
+g.clear();
+setInterval(updateScreen, 1000);
