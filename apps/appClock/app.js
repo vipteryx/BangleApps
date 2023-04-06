0a8f25a6350f10b226d0cfc17b627afeb8b55e23
naga <thejus11@icloud.com>
2023-04-05 20:10:52 -0700

    new approach thanks chatgpt

--- apps/appClock/app.js
+++ apps/appClock/app.js
@@ -1,38 +1,36 @@
-//get the current time
+
 
 function getTime() {
-	
-	var date = new Date();
-	return{
-		hours: date.getHours(),
-		minutes: date.getMinutes(),
-		seconds: date.getSeconds()
-	};	
-}
-
-//draw the time and progress bar
-
-function drawTime(time) {
-	//clear the screen
-	g.clear();
-	
-	//draw the time
-	g.setFont("Vector",25);
-	var timeString = time.hours + ":" + ("0" + time.minutes).substr(-2) + ":" 
-		+ ("0" + time.seconds).substr(-2), 30, 30);
-	var x = g.centerX(timeString); // calculate center x-coordinate
-	var y = (g.getHeight() / 2) - 10; // subtract half of font size
-	g.drawString(timeString, x, y);
+  var date = new Date();
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var seconds = date.getSeconds();
+  var ampm = hours >= 12 ? 'PM' : 'AM'; // Determine AM or PM
+  hours = hours % 12;
+  hours = hours ? hours : 12; // Convert 0 to 12
+  return {
+    hours: hours,
+    minutes: minutes,
+    seconds: seconds,
+    ampm: ampm
+  };
 }
 
 function drawTime(time) {
   //clear the screen
   g.clear();
-	
-//update the time and draw the screen
-function updateScreen(){
-	var time= getTime();
-	drawTime(time);
+
+  //draw the time
+  g.setFont("Vector", 25);
+  var timeString = time.hours + ":" + ("0" + time.minutes).substr(-2) + ":" + ("0" + time.seconds).substr(-2) + " " + time.ampm;
+  var x = g.getWidth() / 2; // center x-coordinate
+  var y = g.getHeight() / 2 - 10; // subtract half of font size
+  g.drawString(timeString, x, y);
+}
+
+function updateScreen() {
+  var time = getTime();
+  drawTime(time);
 }
 
 // Set up the app
